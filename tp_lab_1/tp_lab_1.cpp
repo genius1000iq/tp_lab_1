2fc8007fa8c547d673792512171b9f37949f063f
Egor Karpov <egorka.karpov.2003@mail.ru>
2023-10-23 14:13:07 +0300

    #Добавлено меню

--- tp_lab_1/tp_lab_1.cpp
+++ tp_lab_1/tp_lab_1.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include "Train.h"
 #include "Cargo.h"
 #include "Car.h"
@@ -7,14 +6,61 @@
 #include "string"
 #include "Keeper.h"
 
-
-int main()
+void main(void)
 {
-
     Keeper four;
-   
-    
-    four.InputFile();
-    four.display();
-    return 0;
+    std::cout << "Enter command:" << std::endl;
+    std::cout << "add_car" << std::endl;
+    std::cout << "add_plane" << std::endl;
+    std::cout << "add_train" << std::endl;
+    std::cout << "delete" << std::endl;
+    std::cout << "input_from_file" << std::endl;
+    std::cout << "output_to_file" << std::endl;
+    std::cout << "display" << std::endl;
+    std::cout << "exit" << std::endl;
+    std::cout << "help" << std::endl<<std::endl;
+    while (1)
+    {
+        std::cout << "command>";
+        std::string command;
+        std::cin >> command;
+        if (command == "add_car")
+            four.add(1);
+
+        else if (command == "add_plane")
+            four.add(3);
+
+        else if (command == "add_train")
+            four.add(2);
+        else if (command == "delete")
+        {
+            std::cout << "Enter index of element you want to delete";
+            int index;
+            std::cin >> index;
+            four.remove(index);
+        }
+        else if (command == "input_from_file")
+            four.InputFile();
+        else if (command == "output_to_file")
+            four.OutputFile();
+        else if (command == "display")
+            four.display();
+        else if (command == "help")
+        {
+            std::cout << "Enter command:" << std::endl;
+            std::cout << "add_car" << std::endl;
+            std::cout << "add_plane" << std::endl;
+            std::cout << "add_train" << std::endl;
+            std::cout << "delete" << std::endl;
+            std::cout << "input_from_file" << std::endl;
+            std::cout << "output_to_file" << std::endl;
+            std::cout << "display" << std::endl;
+            std::cout << "exit" << std::endl;
+            std::cout << "help" << std::endl;
+        }
+        else if (command == "exit")
+            return;
+        else std::cout << "unknown command";
+    }
+    return;
 }
